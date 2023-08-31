734effe0126e6693d2465f0a735e071d771ad413
rogeriorioli <carlos.orioli@instivo.com.br>
2023-08-31 16:08:13 -0300

    add video

--- src/app/components/Video/index.tsx
+++ src/app/components/Video/index.tsx
@@ -5,15 +5,7 @@ import styles from "./styles.module.css";
 const Video: React.FC = () => {
   return (
     <div className={styles.videoContainer}>
-      <iframe
-        className={styles.responsiveFrame}
-        width="100%"
-        height="350"
-        loading="lazy"
-        src="https://www.youtube.com/embed/qG394_8FmCc"
-        title="Iniciando um projeto NodeJs , Express com Typescript [PART 1]"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
+      <video className="video" src="/chamada_ws.mov" controls></video>
     </div>
   );
 };
